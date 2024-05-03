59934cda59410eec15bbed04845d5032d7dd5163
Harsh-Rabadiya <128316124+harshrabadiya20@users.noreply.github.com>
2024-05-03 17:37:32 +0530

    student class creating

--- Main.java
+++ Main.java
@@ -7,5 +7,8 @@ public class Main{
         System.out.println(new Date());
         System.out.println("Hello");
         System.out.println("Added one more file");
+
+        Student s1 = new Student();
+        s1.sayHello();
     }
 }
