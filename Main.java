3f1adabc719f57cc3f5c6ed17a74d46481ec47e3
Harsh-Rabadiya <128316124+harshrabadiya20@users.noreply.github.com>
2024-05-03 17:04:26 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        System.out.println("output from java");
+        System.out.println(new Date());
+    }
+}
